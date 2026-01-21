80f7a4575e17d9c1b3a1bb4dc5533fcc0a513aef
LienMacM2 <lienshine@163.com>
2026-01-21 17:56:59 +0800

    更改了sandbox-templates模板，使得构建成功

--- sandbox-templates/gradio-developer/template.ts
+++ sandbox-templates/gradio-developer/template.ts
@@ -1,10 +1,10 @@
 import { Template, waitForPort } from 'e2b'
-
 export const template = Template()
   .fromPythonImage('3.9-slim')
   .aptInstall('curl') // required for waitForPort()
   .pipInstall([
     'gradio',
+    'huggingface_hub==0.23.4',
     'pandas',
     'numpy',
     'matplotlib',
@@ -13,5 +13,21 @@ export const template = Template()
     'plotly',
   ])
   .setWorkdir('/home/user')
-  .copy('app.py', 'app.py')
-  .setStartCmd('gradio app.py', waitForPort(7860))
+  .runCmd(
+    `cat <<'EOF' > /home/user/app.py
+# Gradio 示例
+import gradio as gr
+
+def greet(name, intensity):
+    return "Hello, " + name + "!" * int(intensity)
+
+demo = gr.Interface(
+    fn=greet,
+    inputs=["text", "slider"],
+    outputs=["text"],
+)
+
+demo.launch(server_name="0.0.0.0", server_port=7860)
+EOF`,
+  )
+  .setStartCmd('python app.py', waitForPort(7860))
--- sandbox-templates/vue-developer/template.ts
+++ sandbox-templates/vue-developer/template.ts
@@ -1,13 +1,34 @@
 import { Template, waitForPort } from 'e2b'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 export const template = Template()
   .fromNodeImage('24-slim')
   .aptInstall(['curl', 'git']) // required for waitForPort()
-  .setWorkdir('/home/user/vue-app')
-  .runCmd(
-    'npx nuxi@latest init . --packageManager=npm --gitInit=no -f --modules tailwindcss',
-  )
-  .copy('nuxt.config.ts', 'nuxt.config.ts')
   .setWorkdir('/home/user')
-  .runCmd('mv /home/user/vue-app/* /home/user/ && rm -rf /home/user/vue-app')
+  .runCmd(
+    'npx nuxi@latest init . --template minimal --packageManager=npm --gitInit=no -f --modules tailwindcss',
+  )
+  .runCmd(
+    `cat <<'EOF' > /home/user/nuxt.config.ts
+// https://nuxt.com/docs/api/configuration/nuxt-config
+// @ts-ignore Ignored to pass Vercel deployment
+export default defineNuxtConfig({
+  compatibilityDate: '2024-04-03',
+  devtools: { enabled: false },
+  modules: ['@nuxtjs/tailwindcss'],
+  vite: {
+    server: {
+      allowedHosts: ['.e2b-zulu.dev', '.e2b.dev', '.e2b.app'],
+      hmr: {
+        protocol: 'wss',
+      },
+    },
+  },
+})
+EOF`,
+  )
   .setStartCmd('npm run dev', waitForPort(3000))
